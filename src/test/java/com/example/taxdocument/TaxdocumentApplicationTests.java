80688279e910a4208637e67a407ba15fab7a5498
joaocastro2 <joaocastrocntt06@gmail.com>
2025-12-05 17:08:11 -0300

    feat: initial commit of the NF-e API project

---
+++ src/test/java/com/example/taxdocument/TaxdocumentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taxdocument;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxdocumentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
