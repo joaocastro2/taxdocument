7e802c296ceb58aabdbeb13eb84fb7c2787ddd5b
joaocastro2 <joaocastrocntt06@gmail.com>
2025-12-10 17:49:51 -0300

    "feat: Class for ProductNfe ok"

---
+++ src/main/java/com/example/taxdocument/nfe/model/ProductNfeModel.java
@@ -0,0 +1,15 @@
+package com.example.taxdocument.nfe.model;
+
+public class ProductNfeModel {
+
+    private String codigo;
+
+    private String codigoDeBarrasGtin;
+
+    private String codigoDeBarrasDiferenteGtin;
+
+    private String descricao;
+
+    private String ncm;
+
+}
