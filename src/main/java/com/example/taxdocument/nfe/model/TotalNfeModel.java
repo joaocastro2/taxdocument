90edda6ba215e97618f49187969bd5b526e1d857
joaocastro2 <joaocastrocntt06@gmail.com>
2025-12-17 10:38:08 -0300

    feat: class creation for TotalNFE

--- src/main/java/com/example/taxdocument/nfe/model/TotalNfeModel.java
+++ src/main/java/com/example/taxdocument/nfe/model/TotalNfeModel.java
@@ -1,4 +1,24 @@
-﻿package com.example.taxdocument.nfe.model;
+package com.example.taxdocument.nfe.model;
 
+import com.fincatto.documentofiscal.nfe400.classes.nota.NFNotaInfoICMSTotal;
+import com.fincatto.documentofiscal.nfe400.classes.nota.NFNotaInfoISSQNTotal;
+import com.fincatto.documentofiscal.nfe400.classes.nota.NFNotaInfoRetencoesTributos;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class TotalNfeModel {
+
+    //Total taxes
+    private NFNotaInfoICMSTotal icmsTotal;
+    private NFNotaInfoISSQNTotal issqnTotal;
+    private NFNotaInfoRetencoesTributos retencoesTributos;
+
+    //Final Total
+    private String vNFTot;
+
 }
