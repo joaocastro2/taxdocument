8ce9e1e86723e4ef1fe048e3049b62031200df7b
joaocastro2 <joaocastrocntt06@gmail.com>
2025-12-19 17:27:36 -0300

    ---

---
+++ src/main/java/com/example/taxdocument/nfe/dto/ChargeRequestDto.java
@@ -0,0 +1,9 @@
+package com.example.taxdocument.nfe.dto;
+
+import com.fincatto.documentofiscal.nfe310.classes.nota.NFNotaInfoFatura;
+import org.simpleframework.xml.core.Validate;
+
+public record ChargeRequestDto(
+
+) {
+}
