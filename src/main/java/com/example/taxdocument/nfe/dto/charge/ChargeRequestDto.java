5033d8ff52a0513bb6db40e5a019c9890e6955c6
joaocastro2 <joaocastrocntt06@gmail.com>
2025-12-23 16:13:29 -0300

    refactor: INNER RECORDS implemented

---
+++ src/main/java/com/example/taxdocument/nfe/dto/charge/ChargeRequestDto.java
@@ -0,0 +1,44 @@
+package com.example.taxdocument.nfe.dto.charge;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+public record ChargeRequestDto(
+        @Valid
+        @NotNull(message = "Invoice details are required.")
+        NFInvoiceRequestDto invoiceDto,
+
+        @Valid
+        @NotEmpty(message = "A nota deve ter pelo menos uma parcela")
+        List<NFInstallmentRequestDto> installmentsDto
+) {
+    public record NFInvoiceRequestDto(
+            @NotBlank(message = "Invoice Number is required")
+            String invoiceNumber,
+
+            @NotBlank(message = "Invoice original value is required")
+            String invoiceOriginalValue,
+
+            @NotBlank(message = "Discount Value is required")
+            String invoiceDiscountValue,
+
+            @NotBlank(message = "NetValue is required")
+            String invoiceNetValue
+    ){}
+
+    public record NFInstallmentRequestDto(
+            @NotBlank(message = "Installment number is required")
+            String installmentNumber,
+
+            @NotBlank(message = "Maturity date is required")
+            String maturityDate,
+
+            @NotBlank(message = "Installment value is required")
+            String installmentValue
+    ){}
+
+}
